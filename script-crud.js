48aa52c998115d8cc0be9a6804dce02919408ebd
ispadilha <israel.padilha@gmail.com>
2023-10-18 08:47:00 -0300

    fix: script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,40 +1,46 @@
-const taskListContainer = document.querySelector(".app__section-task-list")
+const taskListContainer = document.querySelector('.app__section-task-list')
+
 const formTask = document.querySelector('.app__form-add-task')
 const toggleFormTaskBtn = document.querySelector('.app__button--add-task')
 const formLabel = document.querySelector('.app__form-label')
-const textarea = document.querySelector('.app__form-textarea')
-const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')
-const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
-const localStorageTarefas = localStorage.getItem('tarefas')
-const activeTaskDescription = document.querySelector('.app__section-active-task-description')
 
+const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')
+
+const taskAtiveDescription = document.querySelector('.app__section-active-task-description')
+
+const textarea = document.querySelector('.app__form-textarea')
+
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+
+const localStorageTarefas = localStorage.getItem('tarefas')
 let tarefas = localStorageTarefas ? JSON.parse(localStorageTarefas) : []
 
-const taskIconSvg = 
-    `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-        fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF" />
-        <path
-            d = "M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-            fill="#01080E"
-        />
-    </svg>`
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`
 
 let tarefaSelecionada = null
 let itemTarefaSelecionada = null
 
 const selecionaTarefa = (tarefa, elemento) => {
+    
     document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
         button.classList.remove('app__section-task-list-item-active')
     })
-    
+
     if (tarefaSelecionada == tarefa) {
         taskAtiveDescription.textContent = null
         itemTarefaSelecionada = null
         tarefaSelecionada = null
         return
     }
-    
+
     tarefaSelecionada = tarefa
     itemTarefaSelecionada = elemento
     taskAtiveDescription.textContent = tarefa.descricao
@@ -47,14 +53,15 @@ const limparForm = () => {
 }
 
 function createTask(tarefa) {
-    const li = document.createElement("li")
-    li.classList.add("app__section-task-list-item")
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
 
-    const svgIcon = document.createElement("svg")
+    const svgIcon = document.createElement('svg')
     svgIcon.innerHTML = taskIconSvg
 
-    const paragraph = document.createElement("p")
-    paragraph.classList.add("app__section-task-list-item-description")
+    const paragraph = document.createElement('p')
+    paragraph.classList.add('app__section-task-list-item-description')
+
     paragraph.textContent = tarefa.descricao
 
     const button = document.createElement('button')
@@ -69,18 +76,18 @@ function createTask(tarefa) {
         li.classList.add('app__section-task-list-item-complete')
     })
 
-    if (tarefa.concluida) {
+    if(tarefa.concluida){
         button.setAttribute('disabled', true)
         li.classList.add('app__section-task-list-item-complete')
     }
 
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
-
+    
     return li
 }
 
-tarefas.forEach((task) => {
+tarefas.forEach(task => {
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
 })
@@ -100,7 +107,7 @@ const updateLocalStorage = () => {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
 }
 
-formTask.addEventListener('submit', (evento => {
+formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
     const task = {
         descricao: textarea.value,
@@ -112,4 +119,4 @@ formTask.addEventListener('submit', (evento => {
 
     updateLocalStorage()
     limparForm()
-}))
+})
